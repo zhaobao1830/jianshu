6290e46adee044e3a0efd78ee95294c034c1f88e
zhaobao1830 <zgt..5219438>
2020-11-17 10:21:09 +0800

    header布局

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,19 +1,34 @@
-import React from "react";
+import React, { Component } from "react";
 import HeaderCss from "./header.module.css";
+import { CSSTransition } from "react-transition-group";
 
-function header () {
-  return (
-    <div className={HeaderCss.header}>
-      <a className={HeaderCss.logo}></a>
-      <div className={HeaderCss.nav}>
-        <div className={HeaderCss.navItem}>首页</div>
-        <div className={HeaderCss.navItem}>下载App</div>
-        <div className={HeaderCss.navItem}>登录</div>
-        <div className={HeaderCss.navItem}>Aa</div>
+class Header extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className={HeaderCss.header}>
+        <a className={HeaderCss.logo}></a>
+        <div className={HeaderCss.nav}>
+          <div className={HeaderCss.navItem}>首页</div>
+          <div className={HeaderCss.navItem}>下载App</div>
+
+         <CSSTransition
+           in={true}
+           timeout={200}
+            >
+              <div className={HeaderCss.inputDiv}>
+                <input className={HeaderCss.input} placeholder={'搜索'}/>
+              </div>
+          </CSSTransition>
+          <div className={HeaderCss.navItem}>登录</div>
+          <div className={HeaderCss.navItem}>Aa</div>
+        </div>
+        <div className={HeaderCss.button}>登录</div>
       </div>
-      <div className={HeaderCss.button}>登录</div>
-    </div>
-  )
+    )
+  }
 }
 
-export default header;
+export default Header;
