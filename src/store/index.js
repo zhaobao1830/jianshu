b2d746943e338551bac0e1d00fdccac1b51e21a6
zhaobao1830 <zgt..5219438>
2020-11-18 09:07:12 +0800

    进行redux-devtools的配置

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
 import { createStore, compose } from "redux";
 import reducer from "./reducer";
 
+// 配置以后就可以使用浏览器的react插件进行调试
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(reducer, composeEnhancers());
