6155e0366d8f0be760008a87f2d68fc4fd849130
zhaobao1830 <zgt..5219438>
2020-11-18 14:15:08 +0800

    引入路由框架react-rputer-dom

--- src/App.js
+++ src/App.js
@@ -2,11 +2,19 @@ import React from "react";
 import { Provider } from 'react-redux';
 import Header from './common/header/index';
 import store from "./store";
+import {
+  BrowserRouter as Router,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
     <Provider store={store}>
       <Header/>
+      <Router>
+        <Route exact path="/" render={() => <div>home</div>}/>
+        <Route exact path="/detail" render={() => <div>detail</div>}/>
+      </Router>
     </Provider>
   );
 }
