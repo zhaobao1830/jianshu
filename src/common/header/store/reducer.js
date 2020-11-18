63a7d67c4d83b7296d5889b6679864858ff097ff
zhaobao1830 <zgt..5219438>
2020-11-18 09:13:09 +0800

    使用immutable.js将state里的数据转换为immutable对象，确保不会在reducer的action方法里直接修改state里的数据

--- src/common/header/store/reducer.js
+++ src/common/header/store/reducer.js
@@ -1,18 +1,30 @@
 import * as constants from './constants'
-const defaultState = {
+import { fromJS } from "immutable";
+
+// 用fromJS把js对象转换成immutable对象
+const defaultState = fromJS({
   focused: false
-};
+});
 
 export default (state = defaultState, action) => {
   if (action.type === constants.SEARCH_FOCUS) {
-    return {
-      focused: true
-    }
+    // 这种写法要手动保证state的数据不被修改，比较麻烦
+    // 所以引入了immutable
+
+    // return {
+    //   focused: true
+    // }
+
+    // immutable对象的set方法，会结合之前immutable对象的值
+    // 和设置的值，返回一个全新的对象
+    return state.set('focused', true)
   }
   if (action.type === constants.SEARCH_BLUR) {
-    return {
-      focused: false
-    }
+    // return {
+    //   focused: false
+    // }
+
+    return state.set('focused', false)
   }
   return state;
 }
